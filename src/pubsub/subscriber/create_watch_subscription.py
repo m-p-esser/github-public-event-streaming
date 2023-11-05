348921a8bbff6ca39fa38925e1728d0062c04594
Marc-Philipp Esser <m-esser@mail.de>
2023-11-05 12:34:47 +0100

    Add Subscription for events from Google Events API in Google Pub/Sub

---
+++ src/pubsub/subscriber/create_watch_subscription.py
@@ -0,0 +1,32 @@
+""" Subscriber for Github Watch Events """
+
+from google.cloud import pubsub_v1
+from icecream import ic
+
+from src.utils import load_env_vars
+
+
+def create_watch_subscription():
+    """Create Subscription in Google Pub/Sub"""
+    env_vars = load_env_vars()
+    project_id = env_vars["GCP_PROJECT_ID"]
+    environment = env_vars["ENV"]
+
+    publisher = pubsub_v1.PublisherClient()
+    subscriber = pubsub_v1.SubscriberClient()
+
+    topic_path = publisher.topic_path(project_id, f"events-{environment}")
+    subscription_path = subscriber.subscription_path(project_id, f"watch-{environment}")
+
+    with subscriber:
+        try:
+            subscription = subscriber.create_subscription(
+                request={"name": subscription_path, "topic": topic_path}
+            )
+            ic(subscription)
+        except Exception as e:
+            ic(e)
+
+
+if __name__ == "__main__":
+    create_watch_subscription()
