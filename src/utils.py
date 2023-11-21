978ce611b7450a66b670eb4f65fc4ad3808ff637
Marc-Philipp Esser <m-esser@mail.de>
2023-11-21 21:34:12 +0100

    add pascal_case_to_lower_case_with_hyphen helper function

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 """ Misc utility functions """
+import re
+
 from dotenv import dotenv_values
 
 
@@ -10,6 +12,14 @@ def load_env_vars() -> dict:
     if len(environment) > 0:
         env_vars.update(environment)
     return env_vars
+
+
+def pascal_case_to_lower_case_with_hyphen(word: str) -> str:
+    """EventType -> event-type"""
+    word_list = re.findall("[A-Z][^A-Z]*", word)
+    word_list_lowercase = [w.lower() for w in word_list]
+    word_lowercase_hyphen = "-".join(word_list_lowercase)
+    return word_lowercase_hyphen
 
 
 if __name__ == "__main__":
