3903fe0663eca07653b85098333f8695b4e026f5
Marc-Philipp Esser <m-esser@mail.de>
2023-11-21 22:34:31 +0100

    add unit test for pascal_case_to_lower_case_with_hyphen

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from src.utils import load_env_vars
+from src.utils import load_env_vars, pascal_case_to_lower_case_with_hyphen
 
 
 def test_load_env_vars_successfully():
@@ -7,5 +7,6 @@ def test_load_env_vars_successfully():
     assert isinstance(env_vars, dict)
 
 
-if __name__ == "__main__":
-    test_load_env_vars_successfully()
+def test_pascal_case_to_lower_case_with_hyphen_sucessfully():
+    modified_word = pascal_case_to_lower_case_with_hyphen("EventType")
+    assert modified_word == "event-type"
