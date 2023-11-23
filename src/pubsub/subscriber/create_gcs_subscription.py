baa741502e95f31052a99cef58f38b7190c0c271
Marc-Philipp Esser <m-esser@mail.de>
2023-11-23 16:33:33 +0100

    add try/catch, store messages in avro binar instead of text files

--- src/pubsub/subscriber/create_gcs_subscription.py
+++ src/pubsub/subscriber/create_gcs_subscription.py
@@ -2,45 +2,66 @@
 
 import hydra
 from google.cloud import pubsub_v1
+from omegaconf import DictConfig
 
 from src.utils import load_env_vars, pascal_case_to_lower_case_with_hyphen
 
 
 @hydra.main(config_path="../../conf", config_name="config")
-def create_gcs_subscriptions(cfg):
+def create_gcs_subscriptions(cfg: DictConfig):
     """Create Google Cloud Storage Subscriptions for Events in Google Pub/Sub"""
 
     publisher = pubsub_v1.PublisherClient()
     subscriber = pubsub_v1.SubscriberClient()
 
-    topic_path = publisher.topic_path(project_id, f"github-events-{environment}")
+    topic_path = publisher.topic_path(project_id, f"github-event-{environment}")
 
-    event_types = [cfg["params"]["event_types"][-1]]
-    for event_type in event_types:
-        event_type_lower_case_hyphen = pascal_case_to_lower_case_with_hyphen(event_type)
+    event_types = cfg.params.event_types
 
-        # GCS Bucket Subscriber
-        subscription_path = subscriber.subscription_path(
-            project_id, f"{event_type_lower_case_hyphen}-gcs-{environment}"
-        )
-
-        cloud_storage_config = pubsub_v1.types.CloudStorageConfig(
-            bucket=f"github-watch-events-{environment}",
-        )
-
-        with subscriber:
-            filter = f'attributes.event_type = "{event_type_lower_case_hyphen}"'
-            subscription = subscriber.create_subscription(
-                request={
-                    "name": subscription_path,
-                    "topic": topic_path,
-                    "cloud_storage_config": cloud_storage_config,
-                    "filter": filter,
-                }
+    with subscriber:
+        for event_type in event_types:
+            event_type_lower_case_hyphen = pascal_case_to_lower_case_with_hyphen(
+                event_type
             )
-            print(
-                f"Created Google Cloud Storage subscription for Event: {event_type}: {subscription}. The following filter rule is active: {filter}"
+            filename_prefix = event_type
+            filename_suffix = ".avro"
+            avro_config = pubsub_v1.types.CloudStorageConfig.AvroConfig(
+                write_metadata=True
             )
+            bucket = f"github-{event_type_lower_case_hyphen}-{environment}"
+
+            try:
+                # GCS Bucket Subscriber
+                subscription_path = subscriber.subscription_path(
+                    project_id, f"{event_type_lower_case_hyphen}-gcs-{environment}"
+                )
+
+                cloud_storage_config = pubsub_v1.types.CloudStorageConfig(
+                    bucket=bucket,
+                    filename_prefix=filename_prefix,
+                    filename_suffix=filename_suffix,
+                    avro_config=avro_config,
+                )
+
+                filter = f'attributes.event_type = "{event_type_lower_case_hyphen}"'
+                subscription = subscriber.create_subscription(
+                    request={
+                        "name": subscription_path,
+                        "topic": topic_path,
+                        "cloud_storage_config": cloud_storage_config,
+                        "filter": filter,
+                    }
+                )
+                print(f"Cloud Storage subscription created: {subscription}.")
+                print(f"Bucket for subscription is: {bucket}")
+                print(f"Prefix is: {filename_prefix}")
+                print(f"Suffix is: {filename_suffix}")
+                print(f"Active Filter: {filter}")
+            except Exception as e:
+                print(
+                    f"An error occured when trying to create Cloud Storage subscription for: {event_type}"
+                )
+                print(e)
 
 
 if __name__ == "__main__":
